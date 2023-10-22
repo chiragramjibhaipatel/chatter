7ff967ec2c56f4d7c5589817f7cc015756073640
Chirag Ramjibhai Patel <69707581+chiragramjibhaipatel@users.noreply.github.com>
2023-10-22 12:07:05 +0530

    remove some unused code

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,7 +5,6 @@ import type { MetaFunction, LoaderFunctionArgs } from "@remix-run/node";
 
 export const loader = async ({} : LoaderFunctionArgs) => {
   const {data} = await supabase.from("messages").select();
-  console.log(data);
   
   return {message: data || []};
 }
@@ -19,11 +18,9 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   const {message} = useLoaderData<typeof loader>();
-  console.log(message);
   
   return (
     <>
-    <h1>Hello</h1>
     <pre>{JSON.stringify(message, null, 2)}</pre>
     </>
   );
